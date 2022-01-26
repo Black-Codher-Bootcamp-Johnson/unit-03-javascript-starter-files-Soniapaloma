c9c492ad6dacfe8bd64dd26063583aad387203d9
Sonia Paloma Osadolor <soniapaloma@MacBook-Pro.local>
2022-01-26 21:00:03 +0000

    commit session 5 task 2 -switch-if task

--- session-5/index.js
+++ session-5/index.js
@@ -1,32 +1,36 @@
-function getGrade(total) {
-    let grade;
+{
+    switch (true) {
+        case (total > 90):
+            grade = 'A+';
+            break;
 
-    if(total > 90) {
-        grade = 'A+'
-    }
-    else if (total > 80) {
-        grade = 'A'
-    }
-    else if (total > 70) {
-        grade = 'B'
-    }
-    else if (total > 60) {
-        grade = 'C'
-    }
-    else if (total > 50) {
-        grade = 'D'
-    }
-    else if (total > 40) {
-        grade = 'E'
-    }
-    else if (total > 30) {
-        grade = 'F'
-    }
-    else {
+        case (total > 80):
+            grade = 'A';
+            break;
+
+        case (total > 70):
+            grade = 'B';
+            break;
+        
+        case (total > 60):
+            grade = 'C';
+            break;
+        
+        case (total > 50):
+            grade = 'D';
+            break;
+        
+        case (total > 40):
+            grade = 'E';
+            break;
+        
+        case (total > 30):
+            grade = 'F';
+            break;
+
+    default:
         grade = 'Fail';
     }
-    
-    console.log("Your grade is " + grade);
-   
+    console.log('Your grade is: ' + grade);
 }
-getGrade(79);
+ getGrade(85);
